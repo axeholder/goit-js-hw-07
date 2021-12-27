ba3df22f1aadff07857661393481be71a320a871
Mykhailo Sokyrko <sokirko.mm@gmail.com>
2021-12-27 20:35:14 +0200

    Лайтбокс
    розметка +
    нпм не подключается -

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const imageContainer = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems.map(({ preview, original, description }) => 
+ `
+ <a class="gallery__item" href="${original}">
+ <img class="gallery__image" src="${preview}" alt="${description}" />
+ </a>
+`).join('');
+
+imageContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+imageContainer.addEventListener('click', onContainerClick);
+
+
+new SimpleLightbox('.gallery__item a', {captionsData:'alt', captionDelay:'250'});
+
+function onContainerClick (event) {
+    if(!event.target.classList.contains("gallery__item")) {
+        return;
+    };
+};       
