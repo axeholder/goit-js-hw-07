d432e0fe4db2413db626de9258a6359b84cc0aed
Mykhailo Sokyrko <sokirko.mm@gmail.com>
2021-12-27 21:09:24 +0200

    Сделано рабочим
    первый вариант не работает, нужно розбираться

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,18 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const imageContainer = document.querySelector('.gallery');
 
-const galleryMarkup = galleryItems.map(({ preview, original, description }) => 
- `
- <a class="gallery__item" href="${original}">
- <img class="gallery__image" src="${preview}" alt="${description}" />
- </a>
-`).join('');
+const container = document.querySelector(".gallery");
 
-imageContainer.insertAdjacentHTML('beforeend', galleryMarkup);
-imageContainer.addEventListener('click', onContainerClick);
+const markupGallery = createGalleryMarkup(galleryItems);
 
+container.insertAdjacentHTML("beforeend", markupGallery);
+
+container.addEventListener('click', onContainerClick);
+
+function createGalleryMarkup (gallery) {
+   return gallery
+    .map (({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}" onclick="event.preventDefault()">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </div>`;
+    })
+    .join(""); 
+};
 
 new SimpleLightbox('.gallery__item a', {captionsData:'alt', captionDelay:'250'});
 
@@ -20,4 +29,28 @@ function onContainerClick (event) {
     if(!event.target.classList.contains("gallery__item")) {
         return;
     };
-};       
+}; 
+
+
+// kakogo to hera ne rabotaet, bydem rozbiratsa
+
+// const imageContainer = document.querySelector('.gallery');
+
+// const galleryMarkup = galleryItems.map(({ preview, original, description }) => 
+//  `
+//  <a class="gallery__item" href="${original}">
+//  <img class="gallery__image" src="${preview}" alt="${description}" />
+//  </a>
+// `).join('');
+
+// imageContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+// imageContainer.addEventListener('click', onContainerClick);
+
+
+// new SimpleLightbox('.gallery__item a', {captionsData:'alt', captionDelay:'250'});
+
+// function onContainerClick (event) {
+//     if(!event.target.classList.contains("gallery__item")) {
+//         return;
+//     };
+// };       
